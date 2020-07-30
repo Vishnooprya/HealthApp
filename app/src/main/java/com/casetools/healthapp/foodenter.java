7ed0ddb451aa8e4418ba1ffe1adba06f7d09c1ea
Vishnooprya <vishnoopriya11@gmail.com>
2020-07-30 13:52:09 +0530

    Initial commit

---
+++ app/src/main/java/com/casetools/healthapp/foodenter.java
@@ -0,0 +1,14 @@
+package com.casetools.healthapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class foodenter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_foodenter);
+    }
+}
